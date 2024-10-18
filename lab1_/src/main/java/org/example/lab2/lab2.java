aefb85703118d9bb69deacce78ec1acb3a352f4f
lamijafatic <lamija.fatic@stu.ibu.edu.ba>
2024-10-18 02:54:45 +0200

    lab 2 finished

--- lab1_/src/main/java/org/example/lab2/lab2.java
+++ lab1_/src/main/java/org/example/lab2/lab2.java
@@ -48,9 +48,88 @@ public class lab2 {
             System.out.println(pocetak);
             pocetak++;
         }
-   /*5*/
+
 
 
         }
+
+        System.out.println(suma(3));
+       printText(3);
+       drawStarsPiramid(7);
+        System.out.println(" ");
+       obrnutaPiramida(8);
+        System.out.println(" ");
+        drawStarsPiramidNumbers(7);
+        /*10*/
+        int brojZaPogoditi=56;
+        int brojilo=0;
+        System.out.println("Guess the number: ");
+        int guess= reader.nextInt();
+        while (guess!=brojZaPogoditi) {
+            if (guess>brojZaPogoditi) {
+                brojilo++;
+                System.out.println("Lower!- This is your " + brojilo + "guess");
+                 guess= reader.nextInt();
+            }
+            else if (guess<brojZaPogoditi) {
+                brojilo++;
+                System.out.println("Higher!- This is your " + brojilo + "guess");
+                guess= reader.nextInt();
+            }
+            }
+        System.out.println("You are correct!");
+        System.out.println("You made "+brojilo+" guesses");
+        }
+
+
+
+    /*5*/
+    public static int suma(int n){
+        int suma=0;
+        for(int i=0;i<=n;i++){
+            suma=suma+(int)Math.pow(2,i);
+        }
+        return suma;
     }
+
+    /*6*/
+    public static void printText (int n){
+        String recenica="In the beginning there were the swamp, the hoe and Java\n";
+        for(int i=0;i<n;i++){
+            System.out.println(recenica);
+        }
+    }
+    /*7*/
+    public static void drawStarsPiramid(int n){
+        for (int i=0;i<=n;i++){
+            for (int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    /*8*/
+    public static void obrnutaPiramida(int n){
+
+        for(int i=n-1;i>=0;i--){
+            for (int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+    /*9*/
+    public static void drawStarsPiramidNumbers(int n){
+        for (int i=0;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.print("\n");
+        }
+    }
+
+
+
 }
+
